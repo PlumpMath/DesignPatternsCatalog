e1a3c67af8be49b26e7a571f93ce3795964c2e14
Roman Blyshchyk <ryblyshc@uwaterloo.ca>
2016-07-10 21:02:03 -0400

    minor comments

--- DesignPatternsCatalogTests/UnitTest1.cs
+++ DesignPatternsCatalogTests/UnitTest1.cs
@@ -3,13 +3,16 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace DesignPatternsCatalogTests
 {
+    /*
+     * Should I organize unit tests via folders( one for each Pattern ) or via files? 
+     */
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
         public void TestMethod1()
         {
-            //more tests - Complete
+            
         }
     }
 }
